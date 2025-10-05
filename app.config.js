913c63221929c76b9a031100834992e2b81ebacc
Arthur Wagenknecht <oarthurdev@gmail.com>
2025-10-05 21:10:43 +0000

    changes

---
+++ app.config.js
@@ -0,0 +1,51 @@
+export default {
+  expo: {
+    name: "expo-on-replit",
+    slug: "expo-on-replit",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    platforms: ["ios", "android", "web"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.anonymous.expoonreplit"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.anonymous.expoonreplit"
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff"
+        }
+      ],
+      "expo-web-browser"
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      router: {
+        origin: false
+      }
+    }
+  }
+};
